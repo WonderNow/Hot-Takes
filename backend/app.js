787d57e330220758978dfd2b8a04c70dda9300c0
Tom Gasche <tomgasche@hotmail.com>
2023-02-09 00:06:59 +0100

    Connexion à la base de données MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://tom:tom123@cluster0.ggs1w0m.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
