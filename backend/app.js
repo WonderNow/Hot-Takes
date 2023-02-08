b1b00adf222f4387cd886cf222151593c536dd44
Tom Gasche <tomgasche@hotmail.com>
2023-02-09 00:59:19 +0100

    Création du controller et de la route pour les utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const userRoutes = require('./routes/user');
+
 mongoose.connect('mongodb+srv://tom:tom123@cluster0.ggs1w0m.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
