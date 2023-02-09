0663602efe573fb26b76270cf5a73439049f2594
Tom Gasche <tomgasche@hotmail.com>
2023-02-09 15:45:27 +0100

    Création de la logique métier du login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
+// Création des utilisateurs dans la base de donnée
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -16,6 +17,24 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+// Vérification de la validité des identifiants fournis par l'utilisateur
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
