a9337bb54dd6948989a74dc40cad8e87688065d3
Tom Gasche <tomgasche@hotmail.com>
2023-02-13 03:09:39 +0100

    Correction du model pour les sauces

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 
 // Utilisation de la méthode Schema de Mongoose afin de créer un schéma de données pour la base de données
 const sauceSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLikes: { type: String, required: true },
+  userDisliked: { type: String, required: true },
 });
 
 // Utilisation de la méthode model afin de transformer le schéma ci-dessus en un modèle utilisable par Mongoose
