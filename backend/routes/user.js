2f855b04192f72f3a00deb518e9eb13eca2b218f
Tom Gasche <tomgasche@hotmail.com>
2023-02-24 17:24:56 +0100

    Ajout de commentaires dans la route user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
+// Crée une instance de routeur Express
 const router = express.Router();
+// Importe le contrôleur des utilisateurs
 const userCtrl = require('../controllers/user');
 
+// Définit la route POST '/signup' pour le routeur et appelle la méthode signup du contrôleur d'utilisateur
 router.post('/signup', userCtrl.signup);
+// Définit la route POST '/login' pour le routeur et appelle la méthode login du contrôleur d'utilisateur
 router.post('/login', userCtrl.login);
 
+// Exporte le routeur pour qu'il puisse être utilisé dans d'autres parties de l'application
 module.exports = router;
